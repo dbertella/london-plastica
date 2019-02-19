3cb90fed81b43228a070259d49fee449f9aa882a
dbertella <daniele.bertella@gmail.com>
2019-02-19 11:00:32 +0000

    Update players with all the info

--- functions/get-all-by-date.js
+++ functions/get-all-by-date.js
@@ -8,32 +8,26 @@ const client = new faunadb.Client({
 
 exports.handler = (event, context, callback) => {
   const date = last(event.path.split('/'))
-  console.log(`Function 'todo-read' invoked. Read date: ${date}`)
+  console.log(`Function 'player-read' invoked. Read date: ${date}`)
   return client
     .query(q.Paginate(q.Match(q.Index('players_by_day'), date)))
-    .then(response => {
+    .then((response) => {
       const playerRefs = response.data
-      console.log('player refs', playerRefs)
+      console.log("Player refs", playerRefs)
       console.log(`${playerRefs.length} players found`)
       // create new query out of player refs. http://bit.ly/2LG3MLg
-      const getAllPlayerDataQuery = playerRefs.map(ref => {
+      const getAllPlayerDataQuery = playerRefs.map((ref) => {
         return q.Get(ref)
       })
       // then query the refs
-      return client.query(getAllplayerDataQuery).then(ret => {
+      return client.query(getAllPlayerDataQuery).then((ret) => {
         return callback(null, {
           statusCode: 200,
           body: JSON.stringify(ret)
         })
       })
-      // console.log('success', response)
-      // return callback(null, {
-      //   statusCode: 200,
-      //   body: JSON.stringify(response)
-      // })
-    })
-    .catch(error => {
-      console.log('error', error)
+    }).catch((error) => {
+      console.log("error", error)
       return callback(null, {
         statusCode: 400,
         body: JSON.stringify(error)
--- src/components/Players.js
+++ src/components/Players.js
@@ -1,23 +1,24 @@
 import React, { useState, useEffect } from 'react'
+import { pick } from 'lodash'
 
 const Players = ({ date }) => {
   const [players, setPlayers] = useState([])
   useEffect(() => {
     fetch(`/.netlify/functions/get-all-by-date/${date}`)
       .then(response => response.json())
-      .then(res => setPlayers(res.data))
+      .then(res => setPlayers(res.map(r => r.data)))
   }, [])
 
-  // const countAvailable = players.filter(([available]) => available).length
+  const countAvailable = players.filter(player => player.available).length
   console.log(players)
   return (
     <div>
-      {/* <h3>Players ({countAvailable} available)</h3>
-      {players.map(([available, name]) => (
+      <h3>Players ({countAvailable} available)</h3>
+      {players.map(({ available, name }) => (
         <div key={name}>
           {name} - {available ? '👍' : '👎'}
         </div>
-      ))} */}
+      ))}
     </div>
   )
 }

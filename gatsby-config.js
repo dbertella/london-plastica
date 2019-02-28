4b124bd919da73b041c0f01a67b62bc5cedcfe12
dbertella <daniele.bertella@gmail.com>
2019-02-28 23:12:42 +0000

    Update meta

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,9 +14,9 @@ module.exports = {
     )
   },
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'London Plastica',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.'
+      'Weekly basket 3 v 3 if we can make it 🏀'
   },
   plugins: [
     'gatsby-plugin-react-helmet',
